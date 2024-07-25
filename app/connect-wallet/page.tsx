1a986ec5e571cde3091e973453ab01cdf14a75f0
danle1308 <dinhhaidan147@gmail.com>
2024-07-25 15:11:15 +0700

    interface connect wallet

---
+++ app/connect-wallet/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import ConnectWallet from '@/views/screens/connectWallet';
+import React from 'react';
+
+export default function callConnectWallet() {
+  return (
+    <ConnectWallet />
+  )
+}
