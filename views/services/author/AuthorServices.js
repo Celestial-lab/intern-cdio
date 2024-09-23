6f5b31cbbed40fc1e4513d1ef2949ad4c8dfd69e
Phan Xuan Tan <PhanXuanTan4522@example.com>
2024-09-23 10:12:06 +0700

    thêm

--- views/services/author/AuthorServices.js
+++ views/services/author/AuthorServices.js
@@ -1,6 +1,7 @@
+
 import axios from '../../axios';
 
-export const getProfileByEmail = async (email) => {
+export const getProfileByEmail = async (email) => { 
     try{
         const response = await axios.get(`/api/profileauthor/${email}`);
         return response.data;
@@ -23,4 +24,45 @@ export const editProfileById = async (id, data) => {
       console.error('loi o edit profile author roi dan oi', error);
       return null;
     }
-  }
+}
+
+export const handleAddProduct = async (id, productname, description, price, status, image, createdAt, updatedAt) => {
+    try {
+      const response = await axios.post('/api/products', {
+        id,
+        productname,
+        description,
+        price,
+        status,
+        image,
+        createdAt,
+        updatedAt,
+      });
+      return response;
+    } catch (error) {
+      console.error('Add Product API error:', error.response || error.message);
+      return null;
+    }
+  };
+
+export const getProductByEmail = async (email) => {
+    try {
+        const response = await axios.get(`/api/products/${email}`); 
+        console.log('data lấy dược', response.data)
+        return response.data;
+    } catch (error) {
+        console.error('Failed to fetch product data:', error);
+        return [];
+    }
+};
+
+export const getImageByEmail = async (email) => {
+    try {
+        const response = await axios.get(`/api/images/email/${email}`); 
+        console.log('data lấy dược', response.data)
+        return response.data;
+    } catch (error) {
+        console.error('Failed to fetch product data:', error);
+        return [];
+    }
+}
